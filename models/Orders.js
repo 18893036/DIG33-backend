d608daa2c0d42723136189db46d07e58edc2a9c7
Adam Walker <18893036@student.curtin.edu.au>
2022-01-18 09:32:09 +1000

    Update Orders.js

--- models/Orders.js
+++ models/Orders.js
@@ -6,10 +6,10 @@ require('mongoose-type-email')
 const ordersSchema= mongoose.Schema({
     name : {type: String , require: true},
     email: {type: String , require: true},
-    userid : {type: String , require: true},
-    orderItems : [],
+//    userid : {type: String , require: true},
+//    orderItems : [],
 //    shippingAddress : {type: Object, require: false},
-    orderAmount : {type:Number , require: true}
+//    orderAmount : {type:Number , require: true}
 //    isDelivered : {type:Boolean , require: false , default: false},
 //    transactionId : {type:String , require: false }
 },{
