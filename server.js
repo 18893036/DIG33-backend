ab93fbf30e8bb3eecfdfa9d9c9c47b90ba2fe694
Adam Walker <18893036@student.curtin.edu.au>
2022-01-18 00:15:25 +1000

    Update server.js

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ const pizzasRouter = require("./routes/pizzas")
 app.use('/pizzas', pizzasRouter)
 
 // orders route
-const ordersRouter = require("./routes/orders")
+const ordersRouter = require("./routes/pizzas")
 app.use('/orders', ordersRouter)
 
 
