734c6c0417a26a57e5eaf3730c96860102470430
Adam Walker <18893036@student.curtin.edu.au>
2022-01-17 23:08:58 +1000

    Still trying

--- server.js
+++ server.js
@@ -69,10 +69,6 @@ app.use('/ciders', cidersRouter)
 const pizzasRouter = require("./routes/pizzas")
 app.use('/pizzas', pizzasRouter)
 
-// orders route
-//const ordersRouter = require("./routes/orders")
-//app.use('/orders', ordersRouter)
-
 // run app (listen on a port).  This actually runs the app.
 app.listen(port, () => {
     console.log("App is running on port ", port)
