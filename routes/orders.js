674424c13c6d2ec755a1c27d90f5b679c2258e24
Adam Walker <18893036@student.curtin.edu.au>
2022-01-18 12:46:13 +1000

    Update orders.js

--- routes/orders.js
+++ routes/orders.js
@@ -2,7 +2,7 @@
 const express = require("express")
 const router = express.Router()
 const Order = require("./../models/Orders")
-//const { v4: uuidv4 } = require("uuid");
+const { v4: uuidv4 } = require("uuid");
 const stripe = require("stripe")(
   'sk_test_51KItkLKHNoE5NY8LKOc2yS59ztSCqXT1dmIZb51LjY05XGE5uVFAdkzKtxi6aSoCvREllrf8VWw5o9tGBxuCh9bo00IaesWPCk'
 );
@@ -64,49 +64,56 @@ router.post('/', (req, res) => {
 
 
 
-
-
 router.post("/placeorder", async (req, res) => {
-    const { subtotal, currentUser, cartItems } = req.body;
- 
-    try {
+  const { token, subtotal, currentUser, cartItems } = req.body;
 
-   
-      
-        const neworder = new Order({
-          name: currentUser.name,
-          email: currentUser.email,
-          userid: currentUser._id,
-          orderItems: cartItems,
-          orderAmount: subtotal
-//          shippingAddress: {
-//           street: token.card.address_line1,
-//            city: token.card.address_city,
-//            country: token.card.address_country,
-//            pincode: token.card.address_zip,
-//          },
-//          transactionId: payment.source.id,
-        });
-  
-        neworder.save();
-  
-        res.send("Order placed successfully");
-    
-      
-    } catch (error) {
-      return res.status(400).json({ message: "Something went wrong" + error });
-    }
-  });
-
-router.post("/getuserorders", async (req, res) => {
-  const { userid } = req.body;
   try {
-    const orders = await Order.find({ userid: userid }).sort({ _id: -1 });
-    res.send(orders);
+    const customer = await stripe.customers.create({
+      email: token.email,
+      source: token.id,
+    });
+
+    const payment = await stripe.charges.create(
+      {
+        amount: subtotal * 100,
+        currency: "inr",
+        customer: customer.id,
+        receipt_email: token.email,
+      },
+      {
+        idempotencyKey: uuidv4(),
+      }
+    );
+
+    if (payment) {
+      const neworder = new Order({
+        name: currentUser.name,
+        email: currentUser.email,
+        userid: currentUser._id,
+        orderItems: cartItems,
+        orderAmount: subtotal,
+        shippingAddress: {
+          street: token.card.address_line1,
+          city: token.card.address_city,
+          country: token.card.address_country,
+          pincode: token.card.address_zip,
+        },
+        transactionId: payment.source.id,
+      });
+
+      neworder.save();
+
+      res.send("Order placed successfully");
+    } else {
+      res.send("Payment failed");
+    }
   } catch (error) {
-    return res.status(400).json({ message: "Something went wrong" });
+    return res.status(400).json({ message: "Something went wrong" + error });
   }
 });
+
+
+
 
 router.get("/getallorders", async (req, res) => {
   try {
