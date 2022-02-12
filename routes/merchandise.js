e4b184082c69e694fd0762c4c393e4cfce846e38
Adam Walker <18893036@student.curtin.edu.au>
2022-02-12 11:04:28 +1000

    Update merchandise.js

--- routes/merchandise.js
+++ routes/merchandise.js
@@ -124,7 +124,7 @@ router.delete('/:id', (req, res) => {
 
 
 router.put("/activatemerchandise/", async (req, res) => {
-    const merchid = req.body.merchid;
+    const merchid = req.params.id;
     try {
         const merch = await Merch.findOne({ _id: merchid });
         console.log(merchid);
